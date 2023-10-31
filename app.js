08e65bf31483241affffa234196166f6835acb7e
damtthy <sabxthiago@gmail.com>
2023-10-31 19:58:17 -0300

    add

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3001, ()=>{
+    console.log('Servidor funcionando en el purto 3001');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
